03b0cea1477f62c7d15eccba671ee13bae509e63
2402zmybie <2402368990@qq.com>
2020-07-05 22:54:44 +0800

    jdbcTemplate

---
+++ src/main/java/com/hr/service/impl/AccountServiceImpl.java
@@ -0,0 +1,42 @@
+package com.hr.service.impl;
+
+import com.hr.dao.IAccountDao;
+import com.hr.domain.Account;
+import com.hr.service.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("accountService")
+public class AccountServiceImpl implements IAccountService {
+
+    @Autowired
+    private IAccountDao accountDao;
+
+
+
+    public Account findAccountById(Integer accountId) {
+        Account account = accountDao.findAccountById(accountId);
+        return account;
+    }
+
+
+    public void transfer(String sourceName, String targetName, Float money) {
+        //查询转出账户
+        Account source = accountDao.findAccountByName(sourceName);
+        //查询转入账户
+        Account target = accountDao.findAccountByName(targetName);
+        //转出账户减钱
+        source.setMoney(source.getMoney() - money);
+        //转入账户加钱
+        target.setMoney(target.getMoney() + money);
+        //更新转出账户
+        accountDao.updateAccount(source);
+//        int i = 1 / 0;
+        //更新转入账户
+        accountDao.updateAccount(target);
+
+
+    }
+}
---
+++ src/test/java/com/hr/test/TxTest.java
@@ -0,0 +1,35 @@
+package com.hr.test;
+
+
+import com.hr.dao.IAccountDao;
+import com.hr.domain.Account;
+import com.hr.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class TxTest {
+
+    @Autowired
+    private IAccountService accountService;
+    @Autowired
+    private IAccountDao accountDao;
+
+    @Test
+    public void testJdbcFindAccountById() {
+        Account account = accountService.findAccountById(1);
+        System.out.println(account);
+    }
+
+    @Test
+    public void testJdbcFindAccountByName() {
+        Account aaa = accountDao.findAccountByName("aaa");
+        System.out.println(aaa);
+    }
+
+
+}
